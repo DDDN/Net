c3177eab1c3ee58b84f84fbb9af6638b3cc57a2d
Lukasz Jaskiewicz <devdone@outlook.com>
2017-08-03 20:59:37 +0200

    folder DDDN.Net.Html renamed to Html

---
+++ Html/HtmlTag.cs
@@ -0,0 +1,29 @@
+﻿/*
+* DDDN.Net.Html.HtmlTag
+* 
+* Copyright(C) 2017 Lukasz Jaskiewicz
+* Author: Lukasz Jaskiewicz (lukasz@jaskiewicz.de)
+*
+* This program is free software; you can redistribute it and/or modify it under the terms of the
+* GNU General Public License as published by the Free Software Foundation; version 2 of the License.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with this program; if not, write
+* to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDN.Net.Html
+{
+    public static class HtmlTag
+    {
+        public static readonly string P = "p";
+        public static readonly string Span = "span";
+        public static readonly string Article = "article";
+    }
+}
